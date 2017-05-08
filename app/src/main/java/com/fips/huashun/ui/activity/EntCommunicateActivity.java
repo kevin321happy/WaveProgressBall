e39243170c92c647dfee60d17071a176a44c5748
qiuma <527892069@qq.com>
2017-05-08 21:26:09 +0800

    提交代码修改

---
+++ app/src/main/java/com/fips/huashun/ui/activity/EntCommunicateActivity.java
@@ -0,0 +1,14 @@
+package com.fips.huashun.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.fips.huashun.R;
+
+public class EntCommunicateActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_ent_communicate);
+  }
+}
